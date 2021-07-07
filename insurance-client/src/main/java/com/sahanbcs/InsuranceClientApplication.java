8e4b53dfdec1c192be4ae1ef93c1725ef8958df7
sahanbcs <sahanepic@gmail.com>
2021-07-07 22:52:08 +0530

    first commit

---
+++ insurance-client/src/main/java/com/sahanbcs/InsuranceClientApplication.java
@@ -0,0 +1,46 @@
+package com.sahanbcs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class InsuranceClientApplication {
+
+
+	@Autowired
+	@Lazy
+	private RestTemplate restTemplate;
+
+	@Value("${insurence.provide.url}")
+	private String uri;
+
+	@GetMapping("/plan")
+	public List<String> plans(){
+		List<String> plan = restTemplate.getForObject(uri,List.class);
+		return   plan;
+	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClientApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
---
+++ insurance-config-server/src/main/java/com/sahanbcs/InsuranceConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.sahanbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableConfigServer
+
+public class InsuranceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceConfigServerApplication.class, args);
+	}
+
+}
